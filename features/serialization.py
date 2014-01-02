ddbc64cb5d593509fe9043b09b1f341c2e5d73e3
cxyzs7 <cxyzs7@gmail.com>
2014-01-01 17:01:22 -0800

    Use temp file

--- features/serialization.py
+++ features/serialization.py
@@ -4,4 +4,26 @@ Created on Jan 1, 2014
 @author: ylou
 '''
 import pickle
+import json
+import tempfile
 
+var = ['hello', [1.0, 2], ['one', 'two'], 'world']
+
+# Native serialization
+with tempfile.NamedTemporaryFile() as temp:
+    serialized_var = pickle.dumps(var)
+    temp.write(serialized_var)
+
+    # deserialize to produce original content
+    temp.seek(0)
+    deserialized_var = pickle.load(temp)
+
+    print deserialized_var
+
+# JSON based serialization
+encoded_var = json.dumps(var)
+
+decoded_var = json.loads(encoded_var)
+
+print encoded_var
+print decoded_var
